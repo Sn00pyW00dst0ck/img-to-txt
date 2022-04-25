9b36fce37c73363d51136d4ba0928ef7e2b88353
Sn00pyW00dst0ck <gabealdous@gmail.com>
2022-04-24 22:45:37 -0400

    useImageData hook implementation

--- src/App.js
+++ src/App.js
@@ -1,71 +1,16 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ImgTextCreator from './components/ImgTextCreator';
 import ImgUploader from './components/ImgUploader';
 import useImageData from './customHooks/useImageData';
 
-const createImage = async (url) =>
-    new Promise((resolve, reject) => {
-        const image = new Image();
-        image.addEventListener('load', () => resolve(image));
-        image.addEventListener('error', error => reject(error));
-        image.setAttribute('crossOrigin', 'anonymous');
-        image.src = url;
-    }
-);
 
 function App() {
-    //States for the img
+    //State for the selected image File object
     const [selectedImage, setSelectedImage] = useState(null);
 
     //Image Data States
-    const [imgDimensions, setImgDimensions] = useState({width: null, height: null});
-    const [imgPixels, setImgPixels] = useState(null);
-
-    //const { imageDimensions, pixelData } = useImageData(selectedImage);
-
-    //Update the img data
-    useEffect(() =>  {
-        //No file selected
-        if (selectedImage == null || selectedImage === undefined)  {
-            setImgDimensions({width: null, height: null});
-            setImgPixels(null);
-            return;
-        }
-
-        const ctx  = document.getElementById("HiddenCanvas").getContext('2d');
-        const img = new Image();
-
-        //Onload function fires once the image loads
-        img.onload = () =>  {
-            //Update the width and height
-            const width = img.width;
-            const height = img.height;
-            setImgDimensions({width: width, height: height});
-
-            //Draw the image to the hidden canvas (required to read the pixel data)
-            document.getElementById("HiddenCanvas").width = width;
-            document.getElementById("HiddenCanvas").height = height;
-            ctx.drawImage(img, 0, 0, width, height);
-            URL.revokeObjectURL(img.src);
-
-            //Read the image pixels
-            const imageData = ctx.getImageData(0, 0, width, height);
-            const data = imageData.data;    //data => [r,g,b,a,...]
-            const pixels = [];
-            for (let i = 0; i < data.length; i += 4) {
-                pixels.push(data[i]);       //R
-                pixels.push(data[i+1]);     //G
-                pixels.push(data[i+2]);     //B
-                pixels.push(data[i+3]);     //A
-            }
-            setImgPixels(pixels);
-        }
-        img.src = URL.createObjectURL(selectedImage);
-    }, [selectedImage]);
-    
-    //console.log(imageDimensions);
-    //console.log(pixelData);
+    const { imageDimensions, pixelData } = useImageData(selectedImage);
 
     return (
         <div className="App">
@@ -77,8 +22,8 @@ function App() {
             <ImgUploader onSelectImageHandler={setSelectedImage}/>
 
             <ImgTextCreator 
-                imageDimensions={imgDimensions}
-                pixelData={imgPixels} />        
+                imageDimensions={imageDimensions}
+                pixelData={pixelData} />        
         </div>
     );
 }
--- src/customHooks/useImageData.js
+++ src/customHooks/useImageData.js
@@ -1,54 +1,49 @@
-import React, { useEffect, useState } from "react";
-
-const createImage = (url) =>
-    new Promise((resolve, reject) => {
-        const image = new Image();
-        image.addEventListener('load', () => resolve(image));
-        image.addEventListener('error', error => reject(error));
-        image.setAttribute('crossOrigin', 'anonymous');
-        image.src = url;
-    }
-);
+import { useEffect, useState } from "react";
 
 const useImageData = (image) =>  {
+    //Some state for image data
     const [imageDimensions, setImageDimensions] = useState({width: null, height: null});
-    const [pixelData, setPixelData] = useState([]);
+    const [pixelData, setPixelData] = useState(null);
 
+    //Runs whenever the image parameter is updated
     useEffect(()=>  {
+        //Return null when image is not passed / undefined / null
         if (image == null || image === undefined)  {
             setImageDimensions({width: null, height: null});
             setPixelData(null);
             return;
         }
 
+        //Get the canvas and create an image object
         const ctx  = document.getElementById("HiddenCanvas").getContext('2d');
         const img = new Image();
 
+        //This fires when the image object src loads in
         img.onload = () =>  {
+            //Update the width and height
             const width = img.width;
             const height = img.height;
             setImageDimensions({width: width, height: height});
+
+            //Draw the image to the hidden canvas (required to read the pixel data)
             document.getElementById("HiddenCanvas").width = width;
             document.getElementById("HiddenCanvas").height = height;
-
             ctx.drawImage(img, 0, 0, width, height);
             URL.revokeObjectURL(img.src);
 
+            //Update the pixel data
             const imageData = ctx.getImageData(0, 0, width, height);
-            const data = imageData.data;
-            //data => [r,g,b,a,...]
+            const data = imageData.data;    //data => [r,g,b,a,...]
             const pixels = [];
             for (let i = 0; i < data.length; i += 4) {
-                pixels.push(data[i]);
-                pixels.push(data[i+1]);
-                pixels.push(data[i+2]);
-                //pixels.push(data[i+3]) for alpha
+                pixels.push(data[i]);       //R
+                pixels.push(data[i+1]);     //G
+                pixels.push(data[i+2]);     //B
+                pixels.push(data[i+3]);     //A
             }
-            console.log(pixels);
-        
-        
+            setPixelData(pixels);
         }
-        img.src = image;
+        img.src = URL.createObjectURL(image);
     }, [image]);
 
     return { imageDimensions, pixelData };
